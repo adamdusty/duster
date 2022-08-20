2722764d027290b5a3c588ffeaa35e0666ff9f75
Adam Dusty <adam.dusty27@gmail.com>
2022-08-19 20:54:26 -0700

    Switched render plugin to Silk.NET because I couldn't figure out how to load opengl functions through SDL-CS.

--- src/Duster.Renderer/RendererPlugin.cs
+++ src/Duster.Renderer/RendererPlugin.cs
@@ -1,17 +1,11 @@
-﻿using DefaultEcs;
+﻿using Silk.NET.Windowing;
+using DefaultEcs;
 using DefaultEcs.System;
 using Duster.Sdk;
-
-using static SDL2.SDL;
 
 namespace Duster.Renderer;
 public class RendererPlugin : IPlugin
 {
-    private SDL_Event _event;
-    private IntPtr _window;
-    private IntPtr _renderer;
-    private World? _world;
-
     public string Name => "Renderer";
 
     public string Description => "Core renderer.";
@@ -26,67 +20,9 @@ public class RendererPlugin : IPlugin
         FixedUpdateSystems = new List<ISystem<float>>();
     }
 
-    private void SignalQuit()
-    {
-        if (_world is not null)
-            _world.Get<ApplicationState>().Quit = true;
-    }
-
     public void Initialize(World world)
     {
-        _world = world;
-        SDL_Init(SDL_INIT_VIDEO);
-        _window = SDL_CreateWindow(
-            "RenderPlugin window",
-            SDL_WINDOWPOS_CENTERED,
-            SDL_WINDOWPOS_CENTERED,
-            1920,
-            1080,
-            SDL_WindowFlags.SDL_WINDOW_SHOWN
-        );
-
-        _renderer = SDL_CreateRenderer(_window, 0, SDL_RendererFlags.SDL_RENDERER_ACCELERATED);
-
-        FrameUpdateSystems.Add(new ActionSystem<float>(s =>
-        {
-            Input();
-
-            SDL_SetRenderDrawColor(_renderer, 255, 0, 0, 255);
-            SDL_RenderClear(_renderer);
-            SDL_RenderPresent(_renderer);
-        }));
-
-
+        FrameUpdateSystems.Add(new WindowSystem(world));
     }
-
-    private void Input()
-    {
-        while (SDL_PollEvent(out _event) != 0)
-        {
-            switch (_event.type)
-            {
-                case SDL_EventType.SDL_QUIT:
-                    SignalQuit();
-                    break;
-                case SDL_EventType.SDL_KEYDOWN:
-                    switch (_event.key.keysym.scancode)
-                    {
-                        case SDL_Scancode.SDL_SCANCODE_ESCAPE:
-                            SignalQuit();
-                            break;
-                        default:
-                            break;
-                    }
-                    break;
-                default:
-                    break;
-            }
-        }
-    }
-
-    public void Dispose()
-    {
-        SDL_DestroyWindow(_window);
-        SDL_Quit();
-    }
+    public void Dispose() { }
 }
---
+++ src/Duster.Renderer/WindowSystem.cs
@@ -0,0 +1,56 @@
+using Silk.NET.Maths;
+using Silk.NET.Input;
+using Silk.NET.Windowing;
+using DefaultEcs;
+using DefaultEcs.System;
+using Duster.Sdk;
+
+namespace Duster.Renderer;
+
+public class WindowSystem : ISystem<float>
+{
+    private IWindow _window;
+    private IInputContext _input;
+    private World _world;
+    public bool IsEnabled { get; set; }
+
+    public WindowSystem(World world)
+    {
+        _world = world;
+
+        var options = WindowOptions.Default;
+        options.Size = new Vector2D<int>(1920, 1080);
+        options.Title = "Hello world from plugin from silk";
+        options.IsEventDriven = false;
+
+        _window = Window.Create(options);
+        _window.Initialize();
+        _input = _window.CreateInput();
+    }
+
+    public void Events()
+    {
+        foreach (var kb in _input.Keyboards)
+        {
+            kb.KeyDown += (kb, k, mod) =>
+            {
+                if (k == Key.Escape)
+                {
+                    _world.Get<ApplicationState>().Quit = true;
+                    _window.Close();
+                }
+            };
+        }
+    }
+    public void Draw() { }
+
+    public void Update(float state)
+    {
+        _window.DoEvents();
+        Events();
+        Draw();
+        _window.DoRender();
+    }
+
+    public void Dispose() { }
+}
